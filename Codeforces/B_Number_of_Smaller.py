2836907947c01458278b986369a02fa01377497e
ABKodes <abiyshiferaw12@gmail.com>
2024-05-09 03:20:19 +0300

    number of smaller

---
+++ Codeforces/B_Number_of_Smaller.py
@@ -0,0 +1,11 @@
+n = int(input())
+m = int(input())
+first = []
+second = []
+for i in range(n):
+    element = int(input)
+    first.append(element)
+for i in range(m):
+    element = int(input)
+    second.append(element)
+print(first)
